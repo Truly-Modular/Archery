c8a61c2016258e1765936941a382be29d8e38788
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-03-06 04:45:55 +0100

    - addons full 1.21, many many many fixes

--- archery-forge/src/main/java/smartin/archery/forge/ArcheryForge.java
+++ archery-forge/src/main/java/smartin/archery/forge/ArcheryForge.java
@@ -1,15 +1,12 @@
 package smartin.archery.forge;
 
-import dev.architectury.platform.forge.EventBuses;
+
+import net.neoforged.fml.common.Mod;
 import smartin.archery.Archery;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Archery.MOD_ID)
 public class ArcheryForge {
     public ArcheryForge() {
-		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(Archery.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-            Archery.init();
+        Archery.init();
     }
 }
